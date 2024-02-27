baaa91a43ba9faf1042dbef5f6613a5e0cf1682d
DivyaGarg0801 <d15961781@gmail.com>
2024-02-27 08:50:10 +0530

    Lenth of a multi-word string

---
+++ lenofmultiwordstring.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[50];
+    gets(a);
+    printf("Length using function:%d\n",strlen(a));
+    int i;
+    for(i=0;a[i]!='\0';i++)
+    {
+    }
+    printf("Length without using function:%d",i);
+    return 0;
+}
